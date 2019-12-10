5e6b9a7d045b58df5fafac1af858929419ba65ba
Jason 'Toolbox' Oettinger <jason@oettinger.email>
2019-12-10 10:04:26 -0500

    2019/separating out IntCodeMachine

--- 2019/2/solution.js
+++ 2019/2/solution.js
@@ -1,85 +1,34 @@
 const { InputFile, TestFile } = require('../util');
 const input = new InputFile(__dirname + '/input.txt');
 const testFile = new TestFile(__dirname + '/test.txt');
+const { IntCodeMachine } = require('../IntCodeMachine');
 
 //Part 1
-class IntCodeMachine {
-    constructor(memory, debug = false, noun, verb) {
-        this.mem = memory;
-        this.pc = 0;
-        this.running = true;
-        this.debug = debug;
-        if (noun) { memory[1] = noun; }
-        if (verb) { memory[2] = verb; }
-    }
-
-    step() {
-        const opcode = this.mem[this.pc];
-        switch (opcode) {
-            case 1:
-                return this.op1();
-            case 2:
-                return this.op2();
-            case 99:
-                return this.op99();
-            default:
-                if (this.debug) { console.log(`invalid opcode: ${opcode}.`); }
-                throw new Error('invalid opcode');
-        }
-    }
-
-    op1() {
-        if (this.debug) { console.log(`mem[${this.mem[this.pc + 3]}] = ${this.mem[this.mem[this.pc + 1]]} + ${this.mem[this.mem[this.pc + 2]]};`); }
-        this.mem[this.mem[this.pc + 3]] = this.mem[this.mem[this.pc + 1]] + this.mem[this.mem[this.pc + 2]];
-        this.pc += 4;
-    }
-
-    op2() {
-        if (this.debug) { console.log(`mem[${this.mem[this.pc + 3]}] = ${this.mem[this.mem[this.pc + 1]]} * ${this.mem[this.mem[this.pc + 2]]};`) }
-        this.mem[this.mem[this.pc + 3]] = this.mem[this.mem[this.pc + 1]] * this.mem[this.mem[this.pc + 2]];
-        this.pc += 4;
-    }
-
-    op99() {
-        return this.running = false;
-    }
-
-    run() {
-        while (this.running) {
-            try {
-                this.step();
-            } catch (err) {
-                return -Infinity;
-            }
-        }
-        return this.mem[0];
-    }
-}
-
 testFile.tests()
     .forEach((test, i) => {
         const memory = test.input.linesAsIntCSVs()[0];
         const machine = new IntCodeMachine(memory);
         const result = machine.run();
-        testFile.check(result, i, true);
+        testFile.check(machine.mem[0], i, true);
     });
 
 const memory = input.linesAsStrings()[0]
     .split(',')
     .map(x => parseInt(x));
 const machine = new IntCodeMachine(memory, false, 12, 2); //12 and 2 as specified in the problem
-const result1 = machine.run();
+machine.run();
+const result1 = machine.mem[0];
 console.log(`Result 1: ${result1}`);
 
 //part 2
-
 const result2 = (() => {
     for (let noun = 0; noun < 100; noun++) {
         for (let verb = 0; verb < 100; verb++) {
             const memory = input.linesAsCSVs()[0]
                 .map(x => parseInt(x));
             const machine = new IntCodeMachine(memory, false, noun, verb);
-            const result = machine.run();
+            machine.run();
+            const result = machine.mem[0];
             if (result === 19690720) { return noun * 100 + verb; }
         }
     }
---
+++ 2019/IntCodeMachine.js
@@ -0,0 +1,115 @@
+exports.IntCodeMachine = class IntCodeMachine {
+    constructor(memory, debug = false, noun, verb) {
+        this.mem = memory;
+        this.pc = 0;
+        this.inputs = [];
+        this.outputs = [];
+        this.debug = debug;
+        this.running = true;
+        if (noun) { memory[1] = noun; }
+        if (verb) { memory[2] = verb; }
+    }
+
+    step() {
+        const instruction = String(this.mem[this.pc])
+            .padStart(5, '0')
+            .split('')
+            .map(char => parseInt(char));
+        if (this.debug) { console.log(`PC: ${this.pc}, Instruction: ${instruction.join('')}`); }
+        if (instruction.length > 5) {
+            throw new Error(`Instruction (${instruction.join('')}) too long`);
+        }
+        //extract opcode
+        const opcode = instruction[3] * 10 + instruction[4];
+        //parse parameter mode
+        const paramAddresses = [1, 2, 3].map(offset =>
+            instruction[3 - offset]
+                ? this.pc + offset
+                : this.mem[this.pc + offset]
+        );
+
+        if (this.debug) { console.log(`Opcode: ${opcode}, ParameterAddresses: ${paramAddresses}`); }
+
+        switch (opcode) {
+            case 1: return this.op1(paramAddresses);
+            case 2: return this.op2(paramAddresses);
+            case 3: return this.op3(paramAddresses);
+            case 4: return this.op4(paramAddresses);
+            case 5: return this.op5(paramAddresses);
+            case 6: return this.op6(paramAddresses);
+            case 7: return this.op7(paramAddresses);
+            case 8: return this.op8(paramAddresses);
+            case 99: return this.running = false;
+            default:
+                throw new Error(`invalid opcode ${opcode}`);
+        }
+    }
+
+    op1(addresses) {
+        this.log(`mem[${addresses[2]}] = ${this.mem[addresses[0]]} + ${this.mem[addresses[1]]};`);
+        this.mem[addresses[2]] = this.mem[addresses[0]] + this.mem[addresses[1]];
+        this.pc += 4;
+    }
+
+    op2(addresses) {
+        this.log(`mem[${addresses[2]}] = ${this.mem[addresses[0]]} * ${this.mem[addresses[1]]};`);
+        this.mem[addresses[2]] = this.mem[addresses[0]] * this.mem[addresses[1]];
+        this.pc += 4;
+    }
+
+    op3(addresses) {
+        if (this.inputs.length > 0) {
+            this.log(`Storing input value ${this.inputs[0]} to ${addresses[0]}.`);
+            this.mem[addresses[0]] = this.inputs.splice(0, 1)[0];
+            this.pc += 2;
+        } else {
+            this.log(`Halting to wait for input.`);
+            //wait for input
+            this.running = false;
+        }
+    }
+
+    op4(addresses) {
+        this.log(`Outputting value ${this.mem[addresses[0]]} to buffer.`);
+        this.outputs.push(this.mem[addresses[0]]);
+        this.pc += 2;
+        if (this.outputs[this.outputs.length - 1] !== 0) { this.running = false; }
+    }
+
+    op5(addresses) {
+        this.log(`If ${this.mem[addresses[1]]} is nonzero, jumping to ${this.mem[addresses[0]]}.`);
+        this.pc = this.mem[addresses[0]] !== 0 ? this.mem[addresses[1]] : this.pc + 3;
+    }
+
+    op6(addresses) {
+        this.log(`If ${this.mem[addresses[1]]} is zero, jumping to ${this.mem[addresses[0]]}.`);
+        this.pc = this.mem[addresses[0]] === 0 ? this.mem[addresses[1]] : this.pc + 3;
+    }
+
+    op7(addresses) {
+        this.log(`mem[${addresses[2]}] = ${this.mem[addresses[0]]} < ${this.mem[addresses[1]]};`);
+        this.mem[addresses[2]] = this.mem[addresses[0]] < this.mem[addresses[1]] ? 1 : 0;
+        this.pc += 4;
+    }
+
+    op8(addresses) {
+        this.log(`mem[${addresses[2]}] = ${this.mem[addresses[0]]} === ${this.mem[addresses[1]]};`);
+        this.mem[addresses[2]] = this.mem[addresses[0]] === this.mem[addresses[1]] ? 1 : 0;
+        this.pc += 4;
+    }
+
+    log(string) {
+        if (this.debug) {
+            console.log(string);
+        }
+    }
+
+    input(num) { this.inputs.push(num); }
+
+    run() {
+        while (this.running) {
+            this.step();
+        }
+        return this.outputs;
+    }
+}
